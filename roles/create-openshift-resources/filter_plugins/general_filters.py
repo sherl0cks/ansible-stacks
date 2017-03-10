6a243efd1b2f43528ccbfbdf56c864708c95b777
Justin Holmes <justinmichaelholmes@gmail.com>
2017-03-09 18:12:10 -0700

    adding new param filter

--- roles/create-openshift-resources/filter_plugins/general_filters.py
+++ roles/create-openshift-resources/filter_plugins/general_filters.py
@@ -10,9 +10,21 @@ def create_key_value_pairs_string(key_value_pairs):
             string += ','
     return string
 
+def create_param_string(key_value_pairs):
+    pairs = []
+    string = ""
+    for key in key_value_pairs:
+        pair = key + '=' + key_value_pairs[key]
+        pairs.append(pair)
+    for i, pair in enumerate(pairs):
+        string += '-p ' + pair
+    return string
+
+
 class FilterModule(object):
     ''' A set of general filters to support OpenShift CLI'''
     def filters(self):
         return {
-            'key_value_pairs_string': create_key_value_pairs_string
+            'key_value_pairs_string': create_key_value_pairs_string,
+            'param_string' : create_param_string
         }
